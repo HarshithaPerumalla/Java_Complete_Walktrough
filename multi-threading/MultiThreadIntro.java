079a3fdf96af436b1413c6f2c769b463d58b777b
HarshithaPerumalla <harshu0989@gmail.com>
2024-08-26 10:38:23 +0530

    updated

---
+++ multi-threading/MultiThreadIntro.java
@@ -0,0 +1,11 @@
+/*
+ * 1.Ways to Define thread
+ * 2.Getiing and setting Name of Thread
+ * 3.Thread Priorities(Imp)
+ * 4.Methods to prevent THread Execution
+ * 5.Synchronization
+ * 6.InterThread Communication
+ * 7.Dead LOck
+ * 8.Demon Thread
+ * 9.MultiThreading enchancement
+ */
